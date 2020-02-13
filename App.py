6b35183ca413a80bfa466b4c780dd25baaa117db
Mujahed <44284154+shaikmujahed@users.noreply.github.com>
2020-02-13 14:43:08 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,1 +1,16 @@
 
+# importing Database from database file
+from database import Database
+# importing User() class from user file
+from user import User
+# connecting to database
+Database.initialise(database ='postgres',user='postgres',password='postgre_sql',host='localhost')
+
+# creating a user
+my_user = User(None,'microsoft@gmail.com','oracle','oracle')
+# saving user to database
+my_user.save_to_db()
+# loading user from database by email
+user_from_db = User.load_from_db_by_email("microsoft@gmail.com")
+# printing out user
+print(user_from_db)
